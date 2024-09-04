5ad7adfb59c3987122f885d361f750b1894860e9
Cryptid09 <154722160+Cryptid09@users.noreply.github.com>
2024-09-04 21:49:23 +0530

    cors added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://buildmynotes.com"],
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   },
@@ -35,7 +35,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://buildmynotes.com"],
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   })
